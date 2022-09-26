07f38f02128a7b339884d99cd55244782fe3ee3b
suhongkim98 <kshz9815@naver.com>
2022-09-26 10:59:18 +0900

    feat: add default dependency

---
+++ src/main/java/com/devnus/belloga/firebase/common/config/KafkaTopicConfig.java
@@ -0,0 +1,19 @@
+package com.devnus.belloga.firebase.common.config;
+
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaTopicConfig {
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String BOOTSTRAP_SERVERS;
+}
+
+
+
