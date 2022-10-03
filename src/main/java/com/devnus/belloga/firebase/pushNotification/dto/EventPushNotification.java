c23111a84cf8c5eb04843b7081d4543acdec0898
suhongkim98 <kshz9815@naver.com>
2022-10-03 19:49:15 +0900

    feat: add subscribeType field to message dto

--- src/main/java/com/devnus/belloga/firebase/pushNotification/dto/EventPushNotification.java
+++ src/main/java/com/devnus/belloga/firebase/pushNotification/dto/EventPushNotification.java
@@ -3,10 +3,13 @@ package com.devnus.belloga.firebase.pushNotification.dto;
 import com.devnus.belloga.firebase.pushNotification.domain.AndroidPriority;
 import com.devnus.belloga.firebase.pushNotification.domain.ApnsPriority;
 import com.devnus.belloga.firebase.pushNotification.domain.ApnsPushType;
+import com.devnus.belloga.firebase.pushNotification.domain.SubscribeType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 public class EventPushNotification {
     @Builder
@@ -14,6 +17,7 @@ public class EventPushNotification {
     @NoArgsConstructor
     @AllArgsConstructor
     public static class Message {
+        private List<SubscribeType> subscribeTypes; //보내고자하는 구독 유형 리스트
         private String title;
         private String body;
         private String clickLink;
